aba952ebd9dc396d8592e042cd4772d1f9ffaf9f
AbeerAljohanii <abeeralialjohani@gmail.com>
2024-09-19 15:22:40 +0300

    Add customer endpoints: GET, POST, PUT, DELETE with additional requirements

--- src/Entities/Customers.cs
+++ src/Entities/Customers.cs
@@ -1,9 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Backend_Teamwork.src.Entities
 {
-    public class Customers { }
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
