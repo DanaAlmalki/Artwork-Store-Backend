af78e7969d650da8363c85cfdec4faf016408295
DanaAlmalki <danaalmalkexvii@gmail.com>
2024-09-23 22:54:13 +0300

    created Artwork entity class

---
+++ src/Entities/Artwork.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Teamwork.src.Controllers
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
