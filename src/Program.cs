e16e5cc529de2c4c1197bf109b362f738a5eb144
AbeerAljohanii <abeeralialjohani@gmail.com>
2024-09-24 13:26:24 +0300

    Add controller service injection and mapping in the application startup

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,11 @@
+using Backend_Teamwork.src.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
