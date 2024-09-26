10404e9d49d4463264d410ea000e82911784792e
DanaAlmalki <danaalmalkexvii@gmail.com>
2024-09-26 14:00:33 +0300

    added mapper class + folders for DTO, Services

---
+++ src/DTO/ArtworkDTO.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Teamwork.src.DTO
+{
+    public class ArtworkDTO
+    {
+        // create Artwork
+        public class ArtworkCreateDto
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+
+        // read data (get data)
+        public class ArtworkReadDto
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+
+        // update
+        public class ArtworkUpdateDTO{
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+    }
+}
---
+++ src/Utils/MapperProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Backend_Teamwork.src.Entities;
+using static Backend_Teamwork.src.DTO.ArtworkDTO; // <<< add this
+
+namespace Backend_Teamwork.src.Utils
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Artwork, ArtworkReadDto>();
+            CreateMap<ArtworkCreateDto, Category>();
+            CreateMap<ArtworkUpdateDTO, Category>().
+            ForAllMembers(opts => opts.Condition((src, dest, srcProperty) => srcProperty != null));
+        }
+    }
+}
