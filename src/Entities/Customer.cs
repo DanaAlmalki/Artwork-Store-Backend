206ae6c13ce19aeda72e253b1368c72975ec5f1a
AbeerAljohanii <abeeralialjohani@gmail.com>
2024-09-26 13:59:54 +0300

    fix: Change variable type from int to Guid for customer and order

--- src/Entities/Customer.cs
+++ src/Entities/Customer.cs
@@ -2,7 +2,7 @@ namespace Backend_Teamwork.src.Entities
 {
     public class Customer
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
