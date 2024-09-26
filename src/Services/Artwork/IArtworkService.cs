05d7b34122a2e70af78daa889dfe5e7a69e5e25d
DanaAlmalki <danaalmalkexvii@gmail.com>
2024-09-26 14:14:07 +0300

    finished IArtworkService Interface

--- src/Services/Artwork/IArtworkService.cs
+++ src/Services/Artwork/IArtworkService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static Backend_Teamwork.src.DTO.ArtworkDTO;
 
 namespace Backend_Teamwork.src.Services.Artwork
 {
     public interface IArtworkService
     {
-        
+        Task<ArtworkReadDto> CreateOneAsync(ArtworkCreateDto artwork);
+        Task<List<ArtworkReadDto>> GetAllAsync();
+        Task<ArtworkReadDto> GetByIdAsync(Guid id);
+        Task<bool> DeleteOneAsync(Guid id);
+        Task<bool> UpdateOneAsync(Guid id, ArtworkUpdateDTO updateArtwork);
     }
 }
