c08fb465cfbd79a827e5044f0d0a231f8588427c
AbeerAljohanii <abeeralialjohani@gmail.com>
2024-09-30 12:04:54 +0300

    feat: Implement authentication and authorization; note that createAdmin function is not working and UsersController functions are untested.

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Backend_Teamwork.src.Database;
 using Backend_Teamwork.src.Repository;
 using Backend_Teamwork.src.Services.artist;
@@ -6,7 +7,10 @@ using Backend_Teamwork.src.Services.category;
 using Backend_Teamwork.src.Services.customer;
 using Backend_Teamwork.src.Services.user;
 using Backend_Teamwork.src.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Npgsql;
 using static Backend_Teamwork.src.Entities.User;
 
@@ -32,6 +36,35 @@ builder.Services.AddScoped<ICustomerService, CustomerService>().AddScoped<Custom
 builder.Services.AddScoped<IArtistService, ArtistService>().AddScoped<ArtistRepository>();
 builder.Services.AddScoped<IArtworkService, ArtworkService>().AddScoped<ArtworkRepository>();
 builder.Services.AddScoped<IUserService, UserService>().AddScoped<UserRepository>();
+
+// add logic for authentication
+builder
+    .Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(Options =>
+    {
+        Options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+            ),
+        };
+    });
+
+// Athorization for Admin
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
 
 // add controllers
 builder.Services.AddControllers();
