06663e7fe68824046360077bfa9fe5bb10ef5ff6
bashaer310 <bashaer_alhothali@outlook.sa>
2024-10-04 20:27:35 +0300

    Update some files

--- src/DTO/UserDTO.cs
+++ src/DTO/UserDTO.cs
@@ -11,7 +11,7 @@ namespace Backend_Teamwork.src.DTO
             public string? PhoneNumber { get; set; }
             public string? Email { get; set; }
             public string? Password { get; set; }
-            public UserRole Role { get; set; } = UserRole.Customer; // Default to Customer
+            public UserRole? Role { get; set; } = UserRole.Customer; // Default to Customer
             public byte[]? Salt { get; set; }
 
             // Artist-specific properties (optional)
